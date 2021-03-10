4af92bb1d99de550f471b39ca40fbc85b756c4f4
Akim <qlikovsky@gmail.com>
2021-03-10 14:49:37 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
+exports.min = function min(array) {
+  if (!array || array.length === 0) {
+      return 0;
+  }
 
-exports.min = function min (array) {
-  return 0;
+  let result = Math.min(...array);
+  return result;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (!array || array.length === 0) {
+      return 0;
+  }
+
+  let result = Math.max(...array);
+  return result;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (!array || array.length === 0) {
+      return 0;
+  }
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+  }
+  return sum / array.length;
 }
